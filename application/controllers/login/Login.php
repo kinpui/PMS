1ff71461155b7d0f92905373d35a11f6607cb17a
kin <cheongkinpui@163.com>
2016-01-20 18:27:34 +0800

    添加登录验证文件

---
+++ application/controllers/login/Login.php
@@ -0,0 +1,21 @@
+<?php 
+/** 
+ * 
+ * Author: smaij 
+ * 
+ * URL : http://www.smaij.com 
+ * 
+ * Created in: 2016-01-20 18:25
+ * 
+ * Description: 
+ * 用户登录验证
+ *  
+ * Update Content: 
+ * 
+ **/ 
+
+class Login extends CI_controller{
+
+
+
+}
