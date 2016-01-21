679286858dec96d076f24f292b4028de3532c5d5
kin <cheongkinpui@163.com>
2016-01-21 20:38:00 +0800

    去除index.php

---
+++ application/helpers/func_helper.php
@@ -0,0 +1,20 @@
+<?php 
+/** 
+ * 
+ * Author: smaij 
+ * 
+ * URL : http://www.smaij.com 
+ * 
+ * Created in: 2016-01-21 17:01
+ * 
+ * Description: 
+ * 扩展函数库 
+ * 用于放置系统各公用函数
+ * Update Content: 
+ * 
+ **/ 
+
+function load_css($css_url){
+  echo 'ss';
+}
+
